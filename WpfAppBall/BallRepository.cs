b0ee1bce825b200331fb269e0312407f135f457d
Teslenko73 <dmytroteslenko73@gmail.com>
2026-03-09 12:19:09 +0100

    pilka

---
+++ WpfAppBall/BallRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppBall
+{
+    internal class BallRepository : IBallApi
+    {
+        public Ball CreateBall()
+        {
+            return new Ball { X = 100, Y = 100 };
+        }
+    }
+}
---
+++ WpfAppBall/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WpfAppBall
+{
+    public partial class MainWindow : Window
+    {
+        private BallLogic _logic;
+        private DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BallRepository data = new BallRepository();
+            _logic = new BallLogic(data.CreateBall());
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(20);
+            _timer.Tick += Update;
+            _timer.Start();
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            _logic.UpdatePosition(GameCanvas.ActualWidth, GameCanvas.ActualHeight);
+
+            Ball b = _logic.GetBall();
+
+            Canvas.SetLeft(BallShape, b.X);
+            Canvas.SetTop(BallShape, b.Y);
+        }
+    }
+}   
