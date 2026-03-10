d266f83fab432bc968cfc1dfd2ba2a2e5d3dda5f
Filip Foryś <254750@edu.p.lodz.pl>
2026-03-10 08:52:22 +0100

    testy

---
+++ WpfAppBall/Testy/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using WpfAppBall;
+
+
+namespace WpfAppBall.Testy
+{
+    public class Test
+    {
+        [Fact] 
+        public void Ball_ShouldMoveCorrect()
+        {
+      
+            var ball = new Ball { X = 100, Y = 100 };
+            var logic = new BallLogic(ball);
+
+            logic.UpdatePosition(500, 500);
+
+            Assert.Equal(105, ball.X);
+            Assert.Equal(105, ball.Y);
+        }
+    }
+}
