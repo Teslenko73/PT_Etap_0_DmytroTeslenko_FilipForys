1c7f742a2c88596768a5f85da23039557e0c6f68
Filip Foryś <254750@edu.p.lodz.pl>
2026-04-21 11:43:20 +0200

    troche kontynuacja

--- WpfAppBall/MainWindow.xaml.cs
+++ WpfAppBall/MainWindow.xaml.cs
@@ -7,30 +7,12 @@ namespace WpfAppBall
 {
     public partial class MainWindow : Window
     {
-        private BallLogic _logic;
-        private DispatcherTimer _timer;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            BallRepository data = new BallRepository();
-            _logic = new BallLogic(data.CreateBall());
-
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromMilliseconds(20);
-            _timer.Tick += Update;
-            _timer.Start();
-        }
-
-        private void Update(object sender, EventArgs e)
-        {
-            _logic.UpdatePosition(GameCanvas.ActualWidth, GameCanvas.ActualHeight);
-
-            Ball b = _logic.GetBall();
-
-            Canvas.SetLeft(BallShape, b.X);
-            Canvas.SetTop(BallShape, b.Y);
+            // Upewnij się, że nazwa po prawej stronie jest identyczna 
+            // z nazwą Twojej klasy w folderze ViewModel
+            this.DataContext = new WpfAppBall.ViewModel.ViewModel();
         }
     }
-}   
+}
