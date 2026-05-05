c9e5f329c013fcd9c69e398cdea45c127b0e5fe1
Filip Foryś <254750@edu.p.lodz.pl>
2026-05-05 11:12:36 +0200

    nsscs

--- WpfAppBall/Testy/Test.cs
+++ WpfAppBall/Testy/Test.cs
@@ -1,42 +1,50 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Xunit;
-//using WpfAppBall;
+﻿using Xunit;
+using WpfAppBall.Data;
 
+namespace WpfAppBall.Testy
+{
+    public class DataApiTests
+    {
+        [Fact]
+        public void Ball_ShouldStayWithinBoundsAfterMove()
+        {
+            // Arrange
+            var dataApi = DataAbstractApi.CreateApi();
+            double width = 100;
+            double height = 100;
 
-//namespace WpfAppBall.Testy
-//{
-//    public class Test
-//    {
-//        [Fact] 
-//        public void Ball_ShouldMoveCorrect()
-//        {
-      
-//            var ball = new Ball { X = 100, Y = 100 };
-//            var logic = new BallLogic(ball);
+            // Tworzymy kulę przez API
+            var ball = dataApi.CreateBall(width, height);
 
-//            logic.UpdatePosition(500, 500);
+            // Act
+            // Wykonujemy serię ruchów
+            for (int i = 0; i < 100; i++)
+            {
+                dataApi.MoveAll(width, height);
+            }
 
-//            Assert.Equal(105, ball.X);
-//            Assert.Equal(105, ball.Y);
-//        }
+            // Assert
+            // Sprawdzamy czy kula nadal jest na planszy (uwzględniając promień)
+            Assert.True(ball.X >= ball.Radius && ball.X <= width - ball.Radius);
+            Assert.True(ball.Y >= ball.Radius && ball.Y <= height - ball.Radius);
+        }
 
-//        [Fact]
-//        public void kolejnytest()
-//        {
-//            var ball = new Ball { X = 468, Y = 100 };
-//            var logic = new BallLogic(ball);
+        [Fact]
+        public void Ball_ShouldChangePositionOnMove()
+        {
+            // Arrange
+            var dataApi = DataAbstractApi.CreateApi();
+            var ball = dataApi.CreateBall(500, 500);
+            double initialX = ball.X;
+            double initialY = ball.Y;
 
-//            // Pierwszy krok powoduje kolizję i odwrócenie prędkości na -5
-//            logic.UpdatePosition(500, 500);
-//            // Drugi krok 473 + (-5) = 468
-//            logic.UpdatePosition(500, 500);
+            // Act
+            dataApi.MoveAll(500, 500);
 
-//            Assert.Equal(468, ball.X); // Kulka wróciła na 468 zamiast lecieć na 478
-//        }
-//    }
-
-//}
+            // Assert
+            // Kula powinna zmienić położenie
+            Assert.NotEqual(initialX, ball.X);
+            Assert.NotEqual(initialY, ball.Y);
+        }
+    }
+}
